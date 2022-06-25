b1193e6352a9144d71ed60838045f740b8afa50a
Hirumi <hilmitsabitulazmi@gmail.com>
2022-06-26 01:20:54 +0700

    -

--- model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	ID       int    `json:"id" gorm:"primaryKey"`
-	NIK      string `json:"nik"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
