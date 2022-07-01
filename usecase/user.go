ab1b4c89812c6bf1ab55690f3b06be49480ea5f5
mzmztelyu <85754066+mzmztelyu@users.noreply.github.com>
2022-07-01 18:42:45 +0700

    repair code usecase

---
+++ usecase/user.go
@@ -0,0 +1,62 @@
+package usecase
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/priyanfadhil/BE-capstone-project-44/config"
+	"github.com/priyanfadhil/BE-capstone-project-44/domain"
+	"github.com/priyanfadhil/BE-capstone-project-44/helper"
+	"github.com/priyanfadhil/BE-capstone-project-44/model"
+)
+
+type svcUser struct {
+	c    config.Config
+	repo domain.AdapterUserRepository
+}
+
+func (s *svcUser) CreateUser(user model.User) error {
+	return s.repo.CreateUsers(user)
+}
+
+func (s *svcUser) UpdateUser(id, idToken int, user model.User) error {
+	fmt.Println(id, idToken)
+	if id != idToken {
+		return fmt.Errorf("error")
+	}
+	return s.repo.UpdateOneUserByID(id, user)
+}
+
+func (s *svcUser) GetAllUsers() []model.User {
+	return s.repo.GetAllUsers()
+}
+
+func (s *svcUser) GetUserByID(id int) (model.User, error) {
+	return s.repo.GetOneUserByID(id)
+}
+
+func (s *svcUser) DeleteUserByID(id int) error {
+	return s.repo.DeleteUserByID(id)
+}
+
+func NewUser(repo domain.AdapterUserRepository, c config.Config) domain.AdapterUser {
+	return &svcUser{
+		repo: repo,
+		c:    c,
+	}
+}
+
+func (s *svcUser) LoginUser(name, password string) (string, int) {
+	user, _ := s.repo.GetOneUserByName(name)
+
+	if (user.Password != password) || (user == model.User{}) {
+		return "", http.StatusUnauthorized
+	}
+
+	token, err := helper.CreateToken(user.ID, user.Email, s.c.JWT_KEY)
+	if err != nil {
+		return "", http.StatusInternalServerError
+	}
+
+	return token, http.StatusOK
+}
