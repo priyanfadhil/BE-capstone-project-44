eb9a6b83b072b3a29218c3a8cb49c3720b236172
Hirumi <hilmitsabitulazmi@gmail.com>
2022-07-08 12:59:36 +0700

    menyelesaikan vaccinationlocation handler

--- controller/handler.go
+++ controller/handler.go
@@ -40,6 +40,36 @@ func UserGroupAPI(e *echo.Echo, conf config.Config) {
 	apiUser.POST("", cont.CreateUserController)
 }
 
+func VaccineStatusGroupAPI(e *echo.Echo, conf config.Config) {
+
+	db := database.InitDB(conf)
+
+	repo := repository.UserMysqlRepository(db)
+
+	svcUser := usecase.NewUser(repo, conf)
+
+	cont := EchoController{
+		svc: svcUser,
+	}
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "your request awesome",
+		})
+	})
+
+	apiUser := e.Group("/users",
+		middleware.Logger(),
+		middleware.CORS(),
+		//m.APIKEYMiddleware,
+	)
+	apiUser.GET("", cont.GetUsersController)
+	apiUser.GET("/:id", cont.GetUserController)
+	apiUser.PUT("/:id", cont.UpdateUserController)
+	apiUser.DELETE("/:id", cont.DeleteUserController)
+	apiUser.POST("", cont.CreateUserController)
+}
+
 func VaccinationLocationGroupAPI(e *echo.Echo, conf config.Config) {
 
 	db := database.InitDB(conf)
