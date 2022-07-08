8e79d54f4ba5610ad8575b85a62ab5f1b1bf1054
mzmztelyu <85754066+mzmztelyu@users.noreply.github.com>
2022-07-08 12:37:33 +0700

    repair handler vaccine

--- controller/handler.go
+++ controller/handler.go
@@ -10,6 +10,36 @@ import (
 	"github.com/priyanfadhil/BE-capstone-project-44/usecase"
 )
 
+func UserGroupAPI(e *echo.Echo, conf config.Config) {
+
+	db := database.InitDB(conf)
+
+	repo := repository.UserMysqlRepository(db)
+
+	svcUser := usecase.NewUser(repo, conf)
+
+	cont := EchoController{
+		svc: svcUser,
+	}
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "your request awesome",
+		})
+	})
+
+	apiUser := e.Group("/users",
+		middleware.Logger(),
+		middleware.CORS(),
+		//m.APIKEYMiddleware,
+	)
+	apiUser.GET("", cont.GetUsersController)
+	apiUser.GET("/:id", cont.GetUserController)
+	apiUser.PUT("/:id", cont.UpdateUserController)
+	apiUser.DELETE("/:id", cont.DeleteUserController)
+	apiUser.POST("", cont.CreateUserController)
+}
+
 func VaccineGroupAPI(e *echo.Echo, conf config.Config) {
 
 	db := database.InitDB(conf)
@@ -27,9 +57,9 @@ func VaccineGroupAPI(e *echo.Echo, conf config.Config) {
 		middleware.CORS(),
 	)
 
-	apiVaccine.GET("", cont.GetVaccineController, middleware.JWT([]byte(conf.JWT_KEY)))
-	apiVaccine.GET("/:id", cont.GetVaccineController, middleware.JWT([]byte(conf.JWT_KEY)))
-	apiVaccine.PUT("/:id", cont.UpdateVaccineController, middleware.JWT([]byte(conf.JWT_KEY)))
-	apiVaccine.DELETE("/:id", cont.DeleteVaccineController, middleware.JWT([]byte(conf.JWT_KEY)))
+	apiVaccine.GET("", cont.GetVaccineController)
+	apiVaccine.GET("/:id", cont.GetVaccineController)
+	apiVaccine.PUT("/:id", cont.UpdateVaccineController)
+	apiVaccine.DELETE("/:id", cont.DeleteVaccineController)
 	apiVaccine.POST("", cont.CreateVaccineController)
 }
